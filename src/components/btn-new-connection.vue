9b4bd651097940c11abd648974a5ec806d45be34
Sietze Keuning <info@kingwebsites.nl>
2020-07-14 15:09:45 +0200

    connection

---
+++ src/components/btn-new-connection.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <el-button type="primary" @click="show=true">New SSH connection</el-button>
+
+    <el-dialog :visible.sync="show" title="New SSH connection" :append-to-body="true">
+      
+      <el-form :model="item" :rules="rules" @submit.native.prevent="save" label-width="140px" ref="form">
+
+        <el-form-item label="Host" prop="host" required>
+          <el-input v-model="item.host" placeholder="sub.domain.com"></el-input>
+        </el-form-item>
+
+        <el-form-item label="Username" prop="username" required>
+          <el-input v-model="item.username" placeholder="user1"></el-input> 
+        </el-form-item>
+
+        <el-form-item label="Database" prop="databasename" required>
+          <el-input v-model="item.databasename" placeholder="db1"></el-input>
+        </el-form-item>
+
+        <el-form-item label="Private key path" prop="key_path" required>
+          <el-input v-model="item.key_path" placeholder="/home/user1/.ssh/id_rsa"></el-input>
+        </el-form-item>
+
+        <div class="flex items-center justify-end">
+          <div v-text="testFeedback" v-if="testFeedback" class="mr-4"></div>
+          <el-button type="default" @click="test">
+            Test
+          </el-button>
+
+          <el-button type="primary" @click="save">
+            Opslaan
+          </el-button>
+        </div> 
+
+      </el-form>
+
+
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import ssh from '@/services/ssh'
+
+export default {
+  data: () => ({
+    show: false,
+    item: {},
+    rules: {
+      host: {required: true },
+      username: {required: true },
+      databasename: {required: true },
+      key_path: {required: true },
+    },
+    testFeedback: null
+  }),
+
+  methods: {
+    async save(){
+      try {
+        await this.$refs.form.validate()
+      } catch (e){
+        this.$message.error('Er is iets mis gegaan')
+      }
+    },
+
+    async test(){
+      const result = ssh.test(this.item)
+      this.testFeedback = result === true ? 'Verbinding succesvol!' : result
+    }
+  }
+}
+</script>
--- src/views/Connections.vue
+++ src/views/Connections.vue
@@ -1,5 +1,106 @@
 <template>
   <div class="about">
-    <h1>Here you will see the different connections</h1>
+    
+    <div class="flex justify-between">
+      <h1 class="mb-8">Here you will see the different connections</h1>
+      <btn-new-connection></btn-new-connection>
+    </div>
+
+    <table class="w-full">
+      <thead>
+        <tr>
+          <th class="text-left">#</th>
+          <th class="text-left">Host</th>
+          <th class="text-left">User</th>
+          <th class="text-left"></th>
+        </tr>
+      </thead>
+
+      <tr v-for="(connection,i) in connections" :key="i">
+        <td>{{ connection.id }}</td>
+        <td>{{ connection.host }}</td>
+        <td>{{ connection.username }}</td>
+        <td class="text-right">
+          <i class="far fa-check-circle"
+            :class="{
+              'text-green-500': connection.status === 'success',
+              'text-red-500': connection.status === 'error',
+              'text-gray-500 hover:text-gray-700': !connection.status
+            }"
+            @click="testConnection(connection, i)"
+          ></i>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
+
+<script>
+const sqlite3 = require('sqlite3')
+const NodeSSH = require('node-ssh')
+import BtnNewConnection from '@/components/btn-new-connection'
+
+export default {
+  components: {
+    BtnNewConnection
+  },
+
+  data: () => ({
+    db: null,
+    connections: []
+  }),
+
+  async created(){
+    this.connect()
+    await this.migrate()
+    await this.query()
+  },
+
+  methods: {
+    connect(){
+      if(this.db && this.db.open)return
+      this.db = new sqlite3.Database('base.db')
+    },
+
+    async migrate(){
+        await this.db.run(`CREATE TABLE if not exists connections (
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          host varchar(191), 
+          username varchar(191), 
+          databasename varchar(191), 
+          key_path varchar(191)
+        )`)
+    },
+
+    async query(){
+      this.db.all('select * from connections', (err, rows) => {
+        this.connections = rows
+      })      
+    },
+
+    async testConnection(connection, i){
+      let ssh = new NodeSSH()
+
+      try{
+        
+        await ssh.connect({
+          host       : connection.host,
+          username   : connection.username,
+          privateKey : connection.key_path
+        })
+
+      } catch(e) {
+        this.$set(this.connections[i], 'status', 'error')
+        this.$message.error('Verbinding niet gelukt')
+        return
+      }
+
+      this.$set(this.connections[i], 'status', 'success')
+      this.$message.success('Verbinding succesvol')
+
+    }
+
+    
+  }
+}
+</script>
